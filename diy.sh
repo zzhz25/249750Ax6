52ff287e5782049cabb374ceeb4943a770ee1f10
终-zz <2497507161@qq.com>
2022-09-25 12:38:43 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -67,3 +67,12 @@ echo 'refresh feeds'
 
 #echo 'enable magic'
 #echo 'src-git helloworld https://github.com/fw876/helloworld'>>./feeds.conf.default
+git clone https://github.com/robbyrussell/oh-my-zsh package/base-files/files/root/.oh-my-zsh
+
+# Install extra plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions package/base-files/files/root/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git package/base-files/files/root/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-completions package/base-files/files/root/.oh-my-zsh/custom/plugins/zsh-completions
+
+# Get .zshrc dotfile
+sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
